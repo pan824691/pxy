1a98a5d261d1c84d38d3181b45bc4680b33c41dd
pan824691 <15814042705@163.com>
2018-10-11 20:13:48 +0800

    省级联动菜单完成

--- dist/assets/js/tetail.js
+++ dist/assets/js/tetail.js
@@ -9,34 +9,68 @@ $('.small-img').find('span').on('click',function () {
     getIndex(index)
 })
 
-$('.centent-right').find('.centent-right-4').find('i').mouseenter(function(){
-   $('.outer').show();
-})
-$('.centent-right').find('.centent-right-4').mouseleave(function(){
-    $('.outer').hide();
- })
+// $('.centent-right').find('.centent-right-4').find('i').mouseenter(function(){
+//    $('.outer').show();
+// })
+// $('.centent-right').find('.centent-right-4').mouseleave(function(){
+//     $('.outer').hide();
+//  })
 
- // 
+var $submenu = document.querySelector('.submenu');
+var $outer = document.querySelector('.outer');
+var $select = $outer.querySelectorAll('select');
+var $button = document.querySelector('#button');
+
+$submenu.onmouseenter = function(){
+    $outer.style.display = 'block';
+
+}
+$button.onclick = function(){
+    var str = '';
+    for(var i = 0; i < $select.length; i++){
+       str += $select[i].value + ' ';   
+
+    }
+$submenu.innerHTML = str;
+   
+        $outer.style.display = 'none';
+   
+}
+    
+
+
+
+
+
  var getInput = (function(){
 
     return {
-        init:function(ele){
+        init:function(){
             this.$leftBtn = document.querySelector('#leftBtn');
             this.$rightBtn = document.querySelector('#rightBtn');
-            this.$inp = document.querySelector('#inp');
-            _num = 1;
+            this.$inp = document.querySelector('#inp');           
             this.event();
-           
         },
-        event:function(){
-            var _this = thsi;
-            this.$leftBtn.onclick(function(){
-                if(this == _this.$leftBtn){
-                     
+        event:function(e){
+            var _this = this;
+            // 左边按钮点击事件
+            this.$leftBtn.onclick = function(){
+                if(_this.$inp.value > 1){
+                    _this.$inp.value -= 1;
                 }
-            })
+                
+            }
+            // 右边按钮点击事件
+            this.$rightBtn.onclick = function(){              
+                _this.$inp.value = parseFloat(_this.$inp.value) +1;
+            }
+          
+          
         }
-    }
+   
+
+        
+        }
+    
  }())
  
- getInput.init();
