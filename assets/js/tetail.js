09cfeb1903ec33f9f5965554967e359f1295d930
pan824691 <15814042705@163.com>
2018-10-11 16:43:22 +0800

    详情页面完成

--- assets/js/tetail.js
+++ assets/js/tetail.js
@@ -8,3 +8,35 @@ $('.small-img').find('span').on('click',function () {
     index = $(this).index();
     getIndex(index)
 })
+
+$('.centent-right').find('.centent-right-4').find('i').mouseenter(function(){
+   $('.outer').show();
+})
+$('.centent-right').find('.centent-right-4').mouseleave(function(){
+    $('.outer').hide();
+ })
+
+ // 
+ var getInput = (function(){
+
+    return {
+        init:function(ele){
+            this.$leftBtn = document.querySelector('#leftBtn');
+            this.$rightBtn = document.querySelector('#rightBtn');
+            this.$inp = document.querySelector('#inp');
+            _num = 1;
+            this.event();
+           
+        },
+        event:function(){
+            var _this = thsi;
+            this.$leftBtn.onclick(function(){
+                if(this == _this.$leftBtn){
+                     
+                }
+            })
+        }
+    }
+ }())
+ 
+ getInput.init();
